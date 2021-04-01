8f50d29e0c28168b885edccbe3658e577c74afc5
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-04-01 19:42:36 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -11,14 +11,5 @@ namespace TypewiseAlert.Test
       Assert.True(BreachTypeAlert.InferBreach(12, 20, 30) ==
         BreachTypeAlert.BreachType.TOO_LOW);
     }
-    [Fact]
-    public void ReportEmailNotification()
-    {
-      BatteryCharacter batteryChar = new BatteryCharacter();
-      batteryChar.coolingType = PASSIVE_COOLING;
-      batteryChar.brand = "Test";
-      Assert.True(AlertTargetType.CheckAndAlert(AlertTargetType.AlertTarget.TO_EMAIL,AlertTargetType.BatteryCharacter.batteryChar,30) ==
-        "Sent");
-    }
   }
 }
