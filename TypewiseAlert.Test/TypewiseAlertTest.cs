2a522eeabc4cc2e7e905bf4bf089fffb94626ecf
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-04-07 17:49:55 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -17,5 +17,21 @@ namespace TypewiseAlert.Test
             Assert.True(CoolingTypeAlert.ClassifyTemperatureBreach(CoolingTypeAlert.CoolingType.HI_ACTIVE_COOLING, 40) ==
               BreachTypeAlert.BreachType.NORMAL);
      }
+    [Fact]
+    public static void CheckAndAlertFunctionalityTests()
+    {
+            try
+            {
+                BatteryCharacter batterychar = new BatteryCharacter();
+                batterychar.brand = "BOSCH";
+                batterychar.coolingType = CoolingTypeAlert.CoolingType.HI_ACTIVE_COOLING;
+                AlertTargetType.CheckAndAlert(AlertTarget.TO_CONTROLLER, batterychar, 10);
+                return; // indicates success
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+    }
   }
 }
