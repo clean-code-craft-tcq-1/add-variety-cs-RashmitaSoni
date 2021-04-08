120f54d9da16d5def8b9d4255c570243fde48c33
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-04-08 09:48:07 +0530

    Update DisplayAlertMessages.cs

--- TypewiseAlert/DisplayAlertMessages.cs
+++ TypewiseAlert/DisplayAlertMessages.cs
@@ -4,32 +4,11 @@ using static TypewiseAlert.CoolingTypeAlert;
 
 namespace TypewiseAlert
 {
-    public interface IDisplayContent
+    public static class DisplayBreachTypeInfo
     {
-        void DisplayContent(BreachType BreachType);
-    }
-
-    class DisplayLowBreachTypeInfo : IDisplayContent
-    {
-        public void DisplayContent(BreachType BreachType)
+        public static void DisplayContent(BreachType BreachType)
         {
-            Console.WriteLine("Hi, the temperature is too low\n");
-        }
-    }
-
-    class DsiplayHighBreachTypeInfo : IDisplayContent
-    {
-        public void DisplayContent(BreachType BreachType)
-        {
-            Console.WriteLine("Hi, the temperature is too high\n");
-        }
-    }
-
-    class DisplayNormalStateInfo : IDisplayContent
-    {
-        public void DisplayContent(BreachType BreachType)
-        {
-            Console.WriteLine("Hi, the temperature is in normal state\n");
+            Console.WriteLine("Temperature is {0}\n",BreachType);
         }
     }
 }
